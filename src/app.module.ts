b41a884b4dd024c772ce5230cb3ade08fc63cce1
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-12-21 11:17:39 -0300

    feat: basic typeorm and supabase settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './modules/user/user.module';
 
+/*
+  npm i @nestjs/config
+  npm i @nestjs/typeorm typeorm
+  npm i pg
+*/
+
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow('DATABASE_HOST'),
+        port: configService.getOrThrow('DATABASE_PORT'),
+        username: configService.getOrThrow('DATABASE_USERNAME'),
+        password: configService.getOrThrow('DATABASE_PASSWORD'),
+        database: configService.getOrThrow('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
