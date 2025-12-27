3b64c01a685647060dff5df415051334a1caeaba
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-12-27 12:59:04 -0300

    feat: creating a dto to standardize the login endpoint response

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dtos/Login.dto';
+import { LoginResponseDto } from './dtos/LoginResponse.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
-  public async login(@Body() dto: LoginDto): Promise<string> {
-    return await this.authService.login(dto);
+  public async login(@Body() dto: LoginDto): Promise<LoginResponseDto> {
+    const token = await this.authService.login(dto);
+    return LoginResponseDto.create(token);
   }
 }
