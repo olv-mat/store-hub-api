a711ea3a6ffa66a6e25983b3916f97d7facda7ba
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-02-18 15:26:13 -0300

    feat: add helmet for security-related headers

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { Environments } from './common/enums/environments.enum';
 import { swaggerSetup } from './common/swagger/setup.swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const environment = process.env.NODE_ENV;
   swaggerSetup(app);
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,6 +16,10 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  if (environment === Environments.PRODUCTION) {
+    app.use(helmet()); // Set Security-Related HTTP Headers
+    app.enableCors({}); // Allow Requests From Other Domains
+  }
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
